ba795df525ce044d6c32c71d8c2f8825d03115f6
wdgoii <134388473+wdgoii@users.noreply.github.com>
2024-08-17 21:05:48 -0300

    alter

--- inicio.ps1
+++ inicio.ps1
@@ -1,9 +1,9 @@
 # Invoke-WebRequest -uri "https://encurtador.com.br/ci4Pr" -OutFile c:\users\inicio.ps1
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 # criar drive local 
-remove-item -Path C:\Windows\Temp\240817 -Recurse   
+remove-item -Path C:\Windows\Temp\240817 -Recurse -ErrorAction Continue
 new-item -Path C:\Windows\Temp\240817 -Type Directory
-Remove-PSDrive -Name T1
+Remove-PSDrive -Name T1 -ErrorAction Continue
 New-PSDrive -Name "T1" -PSProvider FileSystem -Root "C:\Windows\Temp\240817\"
 
 
