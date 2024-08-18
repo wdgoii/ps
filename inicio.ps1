9ce0c9dccd24a888cd0e869a3833aaca418b5886
wdgoii <134388473+wdgoii@users.noreply.github.com>
2024-08-18 10:33:46 -0300

    atua

--- inicio.ps1
+++ inicio.ps1
@@ -1,5 +1,5 @@
 # Invoke-WebRequest -uri "https://encurtador.com.br/ci4Pr" -OutFile c:\users\inicio.ps1
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/informacoes.ps1" -OutFile .\info.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/informacoes.ps1" -OutFile .\informacoes.ps1
 # copiar pasta drive mpf => drive local
 # descompactar
