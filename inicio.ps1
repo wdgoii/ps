9b6423fecf63c978c03941bdb3c27a8530e26d7c
wdgoii <134388473+wdgoii@users.noreply.github.com>
2024-08-20 08:16:36 -0300

    invoke-webrequest

--- inicio.ps1
+++ inicio.ps1
@@ -1,4 +1,18 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/add-cert.ps1" -OutFile .\add-cert.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/atalhos.ps1" -OutFile .\atalhos.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/certificate-install.ps1" -OutFile .\certificate-install.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/drivers-naoutilizados.ps1" -OutFile .\drivers-naoutilizados.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/erros-dispositivos.ps1" -OutFile .\erros-dispositivos.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/fixdhcp.ps1" -OutFile .\fixdhcp.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/fixip.ps1" -OutFile .\fixip.ps1
 Invoke-WebRequest -uri "https://wdgoii.github.io/ps/informacoes.ps1" -OutFile .\informacoes.ps1
-# copiar pasta drive mpf => drive local
-# descompactar
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/install-winget.ps1" -OutFile .\install-winget.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/install.ps1" -OutFile .\install.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/install.txt" -OutFile .\install.txt
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/lentidao.ps1" -OutFile .\lentidao.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/printer-add.ps1" -OutFile .\printer-add.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/reset-windowsupdate.ps1" -OutFile .\reset-windowsupdate.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/uninstall.ps1" -OutFile .\uninstall.ps1
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/uninstall.txt" -OutFile .\uninstall.txt
