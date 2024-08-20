90b02099fecc53a2a73f276f198585612484351d
wdgoii <134388473+wdgoii@users.noreply.github.com>
2024-08-20 08:45:32 -0300

    change

--- inicio.ps1
+++ inicio.ps1
@@ -17,4 +17,4 @@ Invoke-WebRequest -uri "https://wdgoii.github.io/ps/lentidao.ps1" -OutFile .\len
 Invoke-WebRequest -uri "https://wdgoii.github.io/ps/printer-add.ps1" -OutFile .\printer-add.ps1
 Invoke-WebRequest -uri "https://wdgoii.github.io/ps/reset-windowsupdate.ps1" -OutFile "".\reset-windowsupdate.ps1"
 Invoke-WebRequest -uri "https://wdgoii.github.io/ps/uninstall.ps1" -OutFile ".\uninstall.ps1"
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/uninstall-all.txt" -OutFile ".\uninstall-all.txt"
+Invoke-WebRequest -uri "https://wdgoii.github.io/ps/uninstall-all.txt" -OutFile .\uninstall-all.txt
