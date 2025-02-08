b22ed16e9cc3ff8c64794730c4adf5c3f9bc50c3
wdgoii <134388473+wdgoii@users.noreply.github.com>
2025-02-08 16:01:58 -0300

    jfdsf

--- eventos.ps1
+++ eventos.ps1
@@ -3,13 +3,13 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 # Script PowerShell para coletar eventos de erro e críticos do dia atual para investigar tela azul.
 
 # Define o dia de hoje para filtrar os eventos
-$hoje = Get-Date -Format "2025-02-08"
+$hoje = Get-Date -Format "yyyy-MM-dd"
 
 # Consulta os logs de eventos de Sistema e Aplicação para eventos de Erro e Crítico no dia de hoje
 Write-Host "Procurando eventos de Erro e Crítico nos logs de Sistema e Aplicação para o dia: $($hoje)"
 Get-WinEvent -FilterHashtable @{
-    LogName = "System", "Application"
-    Level = 1, 2 # 1: Critical, 2: Error
+    LogName = @("System", "Application")
+    Level = @("1", "2") # 1: Critical, 2: Error
     StartTime = (Get-Date -Date $hoje).Date
     EndTime = (Get-Date -Date $hoje).Date.AddDays(1)
 } | Sort-Object TimeCreated -Descending | Format-Table LogName, TimeCreated, ID, LevelDisplayName, Source, Message -AutoSize
