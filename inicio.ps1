bfab5fce1f6b9cd24aba729f7cc862923e9ed4b5
wdgoii <134388473+wdgoii@users.noreply.github.com>
2025-02-08 16:28:40 -0300

    inicio02

--- inicio.ps1
+++ inicio.ps1
@@ -1,24 +1,17 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
-# invoke-webrequest -uri https://bit.ly/3TkFLPh -outfile .\inicio.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/add-cert.ps1" -OutFile .\add-cert.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/atalho.txt" -OutFile .\atalho.txt
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/criar-atalho.ps1" -OutFile .\criar-atalho.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/certificate-install.ps1" -OutFile .\certificate-install.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/drivers-naoutilizados.ps1" -OutFile .\drivers-naoutilizados.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/erros-dispositivos.ps1" -OutFile .\erros-dispositivos.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/fixdhcp.ps1" -OutFile .\fixdhcp.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/fixip.ps1" -OutFile .\fixip.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/informacoes.ps1" -OutFile .\informacoes.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/install-winget.ps1" -OutFile ".\install-winget.ps1"
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/install-winget02.ps1" -OutFile ".\install-winget02.ps1"
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/install.ps1" -OutFile .\install.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/install.txt" -OutFile .\install.txt
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/install-all.txt" -OutFile .\install-all.txt
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/inst-ps.ps1" -OutFile .\inst-ps.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/lentidao.ps1" -OutFile .\lentidao.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/printer-add.ps1" -OutFile .\printer-add.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/printer-changeport.ps1" -OutFile .\printer-changeport.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/reset-winupdate.ps1" -OutFile .\reset-winupdate.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/uninstall.ps1" -OutFile .\uninstall.ps1
-Invoke-WebRequest -uri "https://wdgoii.github.io/ps/uninstall-all.txt" -OutFile .\uninstall-all.txt 
+# URL do diretório onde os arquivos estão listados
+$directoryUrl = "https://wdgoii.github.io/ps/"
+
+# Obter o conteúdo do diretório
+$response = Invoke-WebRequest -Uri $directoryUrl
+
+# Extrair os links dos arquivos
+$links = $response.Links | Where-Object { $_.href -match "\.ps1$|\.txt$" }
+
+# Baixar cada arquivo
+foreach ($link in $links) {
+    $fileName = [System.IO.Path]::GetFileName($link.href)
+    $fileUrl = $directoryUrl + $fileName
+    Invoke-WebRequest -Uri $fileUrl -OutFile ".\$fileName"
+}
