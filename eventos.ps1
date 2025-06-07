8fe2db60e06b5784d77c853591811f6e257ba3b6
wdgoii <134388473+wdgoii@users.noreply.github.com>
2025-06-07 16:29:36 -0300

    250607-inst-ps

--- eventos.ps1
+++ eventos.ps1
@@ -2,18 +2,13 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
 # Script PowerShell para coletar eventos de erro e críticos do dia atual para investigar tela azul.
 
-# Define o dia de hoje para filtrar os eventos
-$hoje = Get-Date -Format "yyyy-MM-dd"
-
-# Consulta os logs de eventos de Sistema e Aplicação para eventos de Erro e Crítico no dia de hoje
-Write-Host "Procurando eventos de Erro e Crítico nos logs de Sistema e Aplicação para o dia: $($hoje)"
-Get-WinEvent -FilterHashtable @{
-    LogName = @("System", "Application")
-    Level = @("1", "2") # 1: Critical, 2: Error
-    StartTime = (Get-Date -Date $hoje).Date
-    EndTime = (Get-Date -Date $hoje).Date.AddDays(1)
-} | Sort-Object TimeCreated -Descending | Format-Table LogName, TimeCreated, ID, LevelDisplayName, Source, Message -AutoSize
-
 Write-Host ""
 Write-Host "Script concluído. Verifique os eventos listados acima para identificar possíveis causas da tela azul."
 Write-Host "Procure por eventos com 'LevelDisplayName' 'Error' ou 'Critical' e examine as colunas 'Source' e 'Message' para mais detalhes."
+
+Get-WinEvent -FilterHashtable @{
+    LogName = "System", "Application"
+    Level = 1, 2 # 1: Critical, 2: Error
+    StartTime = [datetime]::Today
+    EndTime = ([datetime]::Today).Date.AddDays(300)
+    } | Sort-Object TimeCreated -Descending | Format-Table LogName, TimeCreated, ID, LevelDisplayName, Source, Message -Wrap
--- inst-ps.ps1
+++ inst-ps.ps1
@@ -1,4 +1,17 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+<#
+#Set-Location C:\Users
+$uri = "https://aka.ms/install-powershell.ps1"
+Invoke-RestMethod -Uri $uri | Out-File -FilePath C:\users\powershell.ps1
+C:\users\powershell.ps1
+#>
 
-Invoke-WebRequest -uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.4/PowerShell-7.4.4-win-x64.msi" -OutFile .\ps-7.4.4.msi
-#msiexec.exe /package .\ps-7.4.4.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
+$latestLTSMsiUrl = "https://github.com/PowerShell/PowerShell/releases/download/v7.5.1/PowerShell-7.5.1-win-x64.msi" # Altere a URL para a versão desejada
+$outputPath = "C:\Temp\PowerShell.msi"
+
+Invoke-WebRequest -Uri $latestLTSMsiUrl -OutFile $outputPath
+
+# Instala o MSI silenciosamente
+Start-Process msiexec.exe -ArgumentList "/i `"$outputPath`" /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1" -Wait -NoNewWindow
+
+Remove-Item $outputPath
