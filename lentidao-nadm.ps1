b7552af575b4610d123f8c1c03bc5f44d7d00ab9
unknown <wdgoii@gmail.com>
2025-09-06 20:37:52 -0300

    lent-nadm

--- lentidao-nadm.ps1
+++ lentidao-nadm.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
 Clear-Host
 
 Remove-Item -Path "$env:TEMP\*" -Recurse -Force -ErrorAction SilentlyContinue
@@ -10,3 +12,40 @@ Get-ChildItem "$env:APPDATA\Mozilla\Firefox\Profiles\" -Directory | ForEach-Obje
 }
 
 Start-Process "cleanmgr.exe" -ArgumentList "/sagerun:1"
+
+
+
+##pastas temporarias
+get-childitem -Path C:\Windows -recurse -file -ErrorAction Continue SilentlyContinue | Where-Object {$_.PSpath -match "\\Temp\\"} | Remove-Item -force -ErrorAction SilentlyContinue 2>$null
+
+Remove-Item -Path "C:\Windows\System32\spool\PRINTERS\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+Remove-Item -Path HKLM:\SOFTWARE\Policies\Mozilla\Firefox -Recurse
+
+Write-Output "minha conexao sjrp 39,3mbps / 52,7 mbps"
+Start-Process chrome 'https://www.minhaconexao.com.br --incognito'
+taskmgr.exe
+
+get-process | Sort-Object -Property CPU -Descending | Select-Object id, cpu, name, path -First 30
+
+$Vols = Get-Volume
+"There are $($Vols.Count) volumes to process!"
+For ($Cntr = 0 ; $Cntr -lt $Vols.Count; $Cntr++) {
+ Repair-Volume -ObjectId "$($Vols[$($Cntr)].ObjectId)" 2>$null
+}
+
+#get-childitem -Path 'C:\Users\wdgoi\AppData\Local\Google\Chrome\User Data' -recurse -file | Where-Object {$_.PSpath -match "cookies"} | remove-item -Force
+$Us = get-localUser | where-Object {$_.enabled -eq 'True'}
+For ($Cntr = 0 ; $Cntr -lt $Us.Count; $Cntr++) {
+    Get-ChildItem -File -Path "C:\Users\$($Us[$($Cntr)].name)\AppData" | Where-Object { ($_.PSpath -match "\\cache\\") -or ($_.PSpath -match "\\temp\\")  } | Remove-Item -force -ErrorAction SilentlyContinue 2>$null
+}
+
+#saude hd
+Get-CimInstance -Namespace root\wmi -Class MSStorageDriver_FailurePredictStatus | Select-Object reason,active,instancename
+
+# Clear-DnsClientCache
+# dism /online /cleanup-image /scanhealth
+# dism /online /cleanup-image /restorehealth
+# sfc /scannow
+# repair-Volume -DriveLetter c -Scan -Verbose
+# chkdsk /f /r /b
