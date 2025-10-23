7c6266f6b93902ca7ff87af1357866ed89d82727
robo <rwinformaticasjrp@gmail.com>
2025-10-22 21:38:57 -0300

    comandos

--- inst-ps.ps1
+++ inst-ps.ps1
@@ -6,7 +6,7 @@ Invoke-RestMethod -Uri $uri | Out-File -FilePath C:\users\powershell.ps1
 C:\users\powershell.ps1
 #>
 
-$latestLTSMsiUrl = "https://github.com/PowerShell/PowerShell/releases/download/v7.5.2/PowerShell-7.5.2-win-x64.msi" # Altere a URL para a versão desejada
+$latestLTSMsiUrl = "https://github.com/PowerShell/PowerShell/releases/download/v7.5.4/PowerShell-7.5.4-win-x64.msi" # Altere a URL para a versão desejada
 $outputPath = "C:\windows\Temp\PowerShell.msi"
 
 Invoke-WebRequest -Uri $latestLTSMsiUrl -OutFile $outputPath
