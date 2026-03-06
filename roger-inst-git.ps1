75e3faa293d7d8d7ca993d416b28ce4ea8fd3bdb
wdgoi <wdgoii@gmail.com>
2026-03-06 20:06:45 -0300

    estudo-pcq02+roger-inst-git

---
+++ roger-inst-git.ps1
@@ -0,0 +1,7 @@
+mkdir $home\documents\projetos
+#criei codespace 
+#colaborador  rwinformaticasjrp@gmail.com
+# definir chrome como padrao
+# entrar em rwinformaticasjrp
+git clone https://github.com/wdgoii/robo.git
+git checkout roger
